8e7087cf3e3986361780bc469ad1a7afa5444334
jatingoyal11 <jgoyal2004@gmail.com>
2024-03-06 11:30:27 +0530

    text1

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Developer</span> , a passionate individual with a keen eye for web security and a knack for designing visually stunning websites. As a web bug hunter, I immerse myself in the ever-evolving landscape of cybersecurity, constantly on the lookout for vulnerabilities that could compromise the integrity of online platforms."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
